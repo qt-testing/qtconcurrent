a97a869ec67114831b8d82ad154e0828283ab621
Alexander <lsdkfja0398403@gmail.com>
2018-02-22 20:42:09 +0300

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <iostream>
+
+qint64 factorial(qint64 value)
+{
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	QApplication a(argc, argv);
+
+	std::cout << "Hello !\n";
+}
